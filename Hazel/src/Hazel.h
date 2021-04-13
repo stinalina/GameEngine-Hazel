80350be929f4c38ecdac09c8e1df9e3160aa266a
Stina Böhmig <stina.boehmig@gmx.de>
2021-04-13 21:34:31 +0200

    Setup basic Application and EntryPoint

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For use by Hazel applications
+
+#include "Hazel/Application.h"
+
+// ---EntryPoint------------------
+#include "Hazel/EntryPoint.h"
+// -------------------------------
---
+++ Hazel/src/Hazel/EntryPoint.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifdef HZ_PLATFORM_WINDOWS
+
+// extern tells the compiler that this function exits somewhere. The compiler does not care where; 
+// the linker will resolve all of this references to the one definition that it finds in one of the 
+// compiled files -> in our case on the clientSide in SandboxApp.cpp
+extern Hazel::Application* Hazel::CreateApplication();
+
+int main(int argc, char** argv)
+{
+	auto app = Hazel::CreateApplication();
+	app->Run();
+	delete app;
+}
+
+#endif
