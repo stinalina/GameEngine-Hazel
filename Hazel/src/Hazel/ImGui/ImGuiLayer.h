f26ce6fa248361031b96733edeedad8d19af3e2d
Stina Böhmig <stina.boehmig@gmx.de>
2021-04-19 12:26:15 +0200

    ImGui Events

--- Hazel/src/Hazel/ImGui/ImGuiLayer.h
+++ Hazel/src/Hazel/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Hazel/Layer.h"
 
+#include "Hazel/Application.h"
+#include "Hazel/Events/KeyEvent.h"
+#include "Hazel/Events/MouseEvent.h"
+
 namespace Hazel
 {
 	class HAZEL_API ImGuiLayer : public Layer
@@ -17,6 +21,15 @@ namespace Hazel
 
 	private:
 		float m_Time = 0.0f;
+
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 	};
 }
 
