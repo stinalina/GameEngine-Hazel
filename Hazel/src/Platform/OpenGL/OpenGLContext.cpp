f686df15f3e53da1fb55cb669076faaee02cfade
Stina Böhmig <stina.boehmig@gmx.de>
2021-04-22 11:58:21 +0200

    render first triangle

--- Hazel/src/Platform/OpenGL/OpenGLContext.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLContext.cpp
@@ -17,6 +17,8 @@ namespace Hazel
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		HZ_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		HZ_CORE_INFO("OpenGL Renderer: {0} {1} {2}", glGetString(GL_VENDOR), glGetString(GL_RENDERER), glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
