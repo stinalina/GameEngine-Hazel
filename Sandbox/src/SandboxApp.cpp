6215d984a7eb275287fc050424303c8c7c2c8b51
Stina Böhmig <stina.boehmig@gmx.de>
2021-04-28 10:31:20 +0200

    Transforms

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -2,6 +2,8 @@
 
 #include "imgui/imgui.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 class ExampleLayer : public Hazel::Layer
 {
 public:
@@ -42,6 +44,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position; // v for var between the shaders; this goes out
 			out vec4 v_Color;		
@@ -50,7 +53,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -111,12 +114,13 @@ public:
 			//out vec4 v_Color;	
 
 			uniform mat4 u_ViewProjection;	
+			uniform mat4 u_Transform;	
 
 			void main()
 			{
 				v_Position = a_Position;
 				//v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -163,7 +167,17 @@ public:
 
 		Hazel::Renderer::BeginScene(m_Camera);
 		{
-			Hazel::Renderer::Submit(m_Shader2, m_SquareVA);
+			static glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+			for (int y = 0; y < 20; y++)
+			{
+				for (int x = 0; x < 20; x++)
+				{
+					glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+					glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+					Hazel::Renderer::Submit(m_Shader2, m_SquareVA, transform);
+				}
+			}
+
 			Hazel::Renderer::Submit(m_Shader, m_VertexArray);
 			Hazel::Renderer::EndScene();
 		}
