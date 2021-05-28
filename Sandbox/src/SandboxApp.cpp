91da502803bba82b03c1f3dbaccec796bd6ae284
Stina Böhmig <stina.boehmig@gmx.de>
2021-05-28 15:33:31 +0200

    Blending

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -177,6 +177,7 @@ public:
 		m_TextureShader.reset(Hazel::Shader::Create(textureShaderVertexSrc, textureShaderFragmentSrc));
 	
 		m_Texture = Hazel::Texture2D::Create("assets/textures/test.png");
+		m_LogoTexture = Hazel::Texture2D::Create("assets/textures/logo.png");
 
 		std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_TextureShader)->Bind();
 		std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_TextureShader)->UploadUniformInt("u_Texture", 0);
@@ -226,6 +227,8 @@ public:
 
 			m_Texture->Bind();
 			Hazel::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
+			m_LogoTexture->Bind();
+			Hazel::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
 
 			// Triangle
 			//Hazel::Renderer::Submit(m_Shader, m_VertexArray);
@@ -254,7 +257,7 @@ private:
 	Hazel::Ref<Hazel::Shader> m_FlatColorShader, m_TextureShader;
 	Hazel::Ref<Hazel::VertexArray> m_SquareVA;
 
-	Hazel::Ref<Hazel::Texture2D> m_Texture;
+	Hazel::Ref<Hazel::Texture2D> m_Texture, m_LogoTexture;
 
 	Hazel::OrthographicCamera m_Camera;
 	glm::vec3 m_CameraPosition;
