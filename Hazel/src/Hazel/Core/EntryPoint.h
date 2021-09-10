2b77016c0b9bc03261326a4d832821411f545405
Stina <stinaboehmig@gmx.de>
2021-09-10 13:21:50 +0200

    Visual Profiling

--- Hazel/src/Hazel/Core/EntryPoint.h
+++ Hazel/src/Hazel/Core/EntryPoint.h
@@ -12,9 +12,17 @@ int main(int argc, char** argv)
 {
 	Hazel::Log::Init();
 
+	HZ_PROFILE_BEGIN_SESSION("Startup", "HazelProfile-Startup-json");
 	auto app = Hazel::CreateApplication();
+	HZ_PROFILE_END_SESSION();
+
+	HZ_PROFILE_BEGIN_SESSION("Runtime", "HazelProfile-Runtime-json");
 	app->Run();
+	HZ_PROFILE_END_SESSION();
+
+	HZ_PROFILE_BEGIN_SESSION("Shutdown", "HazelProfile-Shutdown-json");
 	delete app;
+	HZ_PROFILE_END_SESSION();
 }
 
 #endif
